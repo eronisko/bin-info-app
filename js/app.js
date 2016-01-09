e1537cd9c0951d39542fcbb21129220d28e383a8
Ernest Walzel <ewalzel@gmail.com>
2016-01-09 17:46:27 +0200

    Filter locations while ignoring diacritics

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function() {
-  angular.module('app', ['ngRoute'])
+  angular.module('app', ['ngRoute', 'angular.filter'])
 
     .config(['$routeProvider',
       function($routeProvider) {
@@ -29,9 +29,29 @@
     .controller('BinLocationDetailController', ['$routeParams',
       function($routeParams) {
         this.location = findLocationByStreet($routeParams.streetName)
-        console.log(this.location);
       }
-    ]);
+    ])
+
+    // Filters locations by street names. The filtering is insensitive to case
+    // and diacritics.
+    .filter('filterByStreet', ['$filter', function($filter) {
+      return function(input, query) {
+        if (!query) {
+          return;
+        }
+
+        var queryForComparison = $filter('latinize')(query).toUpperCase();
+
+        // Custom comparator predicate function
+        expression = function(value, index, array) {
+          var streetForComparison = $filter('latinize')(value.street).toUpperCase();
+          return streetForComparison.indexOf(queryForComparison) >= 0;
+        }
+
+        return $filter('filter')(input, expression);
+      }
+    }])
+    ;
 
     function findLocationByStreet(street) {
       var result = $.grep(locationsData, function(e){ return e.street == street; });
